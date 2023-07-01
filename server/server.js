dee79e67290bff68672d67987faec8d4afe6fb11
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-07-01 14:29:53 +0530

    Update server.js

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "signup"
+})
+
+app.listen(5000, () => {
+    console.log("Running...");
+})
