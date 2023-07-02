481d12294b93e816f086d25edf5192c61719ca3c
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-07-02 21:39:22 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./Home";
+import Register from "./Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/register' element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
