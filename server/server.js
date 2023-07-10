dc6e29bf2743c129c531754ccb0db270704c59a7
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-07-10 18:02:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,6 +23,20 @@ const db = mysql.createConnection({
     database: "signup"
 })
 
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM login WHERE email = ? AND password = ?";
+    db.query(sql, [req.body.email, req.body.password], (err, data) => {
+        if(err) return res.json({Message: "Server Side Error"});
+        if(data.length > 0) {
+            const name = data[0].name;
+            const token = jwt.sign({name}, "our-jsonwebtoken-secret-key", {expiresIn: '1d'});
+            res.cookie('token', token);
+        } else {
+            return res.json({Message: "No Records existed"});
+        }
+    })
+})
+
 app.listen(5000, () => {
     console.log("Running...");
 })
