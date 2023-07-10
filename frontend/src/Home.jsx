15e9d7ecde047d8ffa10fcf419023aaf0456158b
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-07-10 18:31:33 +0530

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
+
+  const [auth, setAuth] = useState(false)
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    axios.get('')
+    .then(res => {
+      if(res.data.Status === "Success"){
+        setAuth(true);
+        setName(res.data.name);
+      }
+    })
+  })
+
   return (
-    <div>
-      
+    <div className='container mt-4'>
+      {
+        auth ? 
+        <div>
+          <h3>You are Authorized {}</h3>
+          <button className='btn btn-danger'>Logout</button>
+        </div>
+        :
+        <div>
+          <h3>{}</h3>
+          <h3>Login Now</h3>
+          <button className='btn btn-primary'>Login</button>
+        </div>
+      }
     </div>
   )
 }
