17c268ba5a4ceea2ee0b25f3a445da1255385d1b
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-07-10 18:56:22 +0530

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
 
   const [auth, setAuth] = useState(false)
   const [name, setName] = useState('')
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     axios.get('')
@@ -12,21 +13,31 @@ function Home() {
       if(res.data.Status === "Success"){
         setAuth(true);
         setName(res.data.name);
+      } else {
+        setAuth(false);
+        setMessage(res.data.Message)
       }
     })
-  })
+  }, [])
+
+  const handleLogout = () => {
+    axios.get('')
+    .then( res => {
+      location.reload(true);
+    }).catch( err => console.log(err))
+  }
 
   return (
     <div className='container mt-4'>
       {
         auth ? 
         <div>
-          <h3>You are Authorized {}</h3>
-          <button className='btn btn-danger'>Logout</button>
+          <h3>You are Authorized {name}</h3>
+          <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
         </div>
         :
         <div>
-          <h3>{}</h3>
+          <h3>{message}</h3>
           <h3>Login Now</h3>
           <button className='btn btn-primary'>Login</button>
         </div>
